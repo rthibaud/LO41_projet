68646d5af80acf1657f92bb4f3ac4391202f5e6c
Chiara Salvoni <chiarasal91@gmail.com>
2014-12-29 20:31:22 +0100

    fonction premier poste ajoute, a revoire les semaphores et la gestion de chaque demande

---
+++ chaineDeProduction(1).h
@@ -0,0 +1,33 @@
+#ifndef _CHAINEDEPRODUCTION_
+#define _CHAINEDEPRODUCTION_
+
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define nbPostes 4
+
+
+//int nbPostes;
+int nbPieces;
+int val;
+
+sem_t *panneauTicket;
+sem_t *zoneCaissePleine;
+sem_t *zoneCaisseVide;
+pthread_t *tid;
+pthread_mutex_t mutex;
+
+
+
+void* creationThread(void* ID);
+void posteDeTravail(int ID);
+
+
+
+
+
+#endif
---
+++ main(1).c
@@ -0,0 +1,38 @@
+#include "chaineDeProduction.h"
+#include <malloc.h>
+
+
+int main (int argc, char* argv[])
+{
+	int num, i;
+	pthread_mutex_init(&mutex,NULL);
+
+	//nbPostes=4;
+
+	tid=(pthread_t *)malloc((nbPostes+1)*sizeof(pthread_t)); 
+
+	panneauTicket=(sem_t*)malloc((nbPostes+1)*sizeof(sem_t));
+	zoneCaissePleine=(sem_t*)malloc((nbPostes+1)*sizeof(sem_t));
+	zoneCaisseVide=(sem_t*)malloc((nbPostes+1)*sizeof(sem_t));
+
+	for(num=0;num<nbPostes+1;num ++)
+	{	
+		sem_init(&panneauTicket[num],0,2); 
+		sem_init(&zoneCaissePleine[num],0,1);
+		sem_init(&zoneCaisseVide[num],0,1);
+		pthread_create(&tid[num],0,(void *(*)())creationThread,(void*)num);
+	}
+	
+	for(num=0;num<nbPostes+1;num++)
+	{
+		pthread_join(tid[num],NULL);
+	}
+	
+	
+	free(tid);
+	free(panneauTicket);
+	free(zoneCaissePleine);
+	free(zoneCaisseVide);
+
+	return 0;
+}
